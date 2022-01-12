6ed55de4ff115bf6d97c8273fa48f0a601bb420d
kvrick <kvrick@hotmail.com>
2022-01-12 11:29:01 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-vfwnuk-ughcds
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-vfwnuk-ughcds)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,22 @@
+//import { Component, OnInit, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+/*export class ProductAlertsComponent implements OnInit {
+  @Input() product!: Product;
+
+  constructor() { }
+
+  ngOnInit(): void {
+ 
+}
+}*/
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
+}
